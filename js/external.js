13dc0712fe37cf6c94210f8821969b08133b2eaa
Casper <clydenberg@gmail.com>
2023-10-05 13:13:03 -0500

    Inline/External JS Excersize PT.2

--- js/external.js
+++ js/external.js
@@ -2,20 +2,37 @@ console.log("Hello from external JavaScript");
 
 //Use the alert function to show a message that says 'Welcome to my Website!'.
 
-//alert("Welcome to my Website");
+alert("Welcome to my Website");
 
 //Use a prompt to ask for the user's favorite color. Use the user's response to alert a message that says that the
 //color entered is your favorite color too.
 
-// let userFavColor = prompt("What's your favorite color?");
-// alert(`My favorite color is also: ${userFavColor}`);
+let userFavColor = prompt("What's your favorite color?");
+alert(`My favorite color is also: ${userFavColor}`);
+
+//When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
+//Use an alert to show the results of each problem.
 
 //You have rented some movies for your kids: The little mermaid (for 3 days),
 //Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it).
 //If price for a movie per day is $3, how much will you have to pay?
 
-//When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
-//Use an alert to show the results of each problem.
+const costOfMovieRental = 3;
+
+let lilMermaidDaysRented = prompt("how many days would you like to rent the little mermaid?");
+let costOfLilMermaid = parseInt(lilMermaidDaysRented) * costOfMovieRental;
+alert(`The rental fee for the lil mermaid will be ${costOfLilMermaid}`);
+
+let broBearDaysRented = prompt("how many days would you like to rent Brother Bear?");
+let costOfBroBear = parseInt(broBearDaysRented) * costOfMovieRental;
+alert(`The rental fee for BrotherBear will be ${costOfBroBear}`);
+
+let herculesDaysRented = prompt("How many days would you like to rent Hercules?");
+let costOfHercules = parseInt(herculesDaysRented) * costOfMovieRental;
+alert(`The rental fee for Hercules will be ${costOfHercules}`);
+
+let totalRentalCost = costOfLilMermaid + costOfBroBear + costOfHercules;
+alert(`Your total cost for all of your movies will be: ${totalRentalCost}`);
 
 //Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different
 //rate per hour.
@@ -23,10 +40,10 @@ console.log("Hello from external JavaScript");
 //You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
 
 const hourlyPayAtGoogle = 400;
-const hourlyPayAtAmazon = 380
-const hourlyPayAtFacebook = 350
+const hourlyPayAtAmazon = 380;
+const hourlyPayAtFacebook = 350;
 
-let workedHoursAtGoogle = prompt("How many hours did you work at google this week?")
+let workedHoursAtGoogle = prompt("How many hours did you work at google this week?");
 let weeklyPayFromGoogle = parseInt(workedHoursAtGoogle) * hourlyPayAtGoogle;
 alert(`Your pay from google this week is: ${weeklyPayFromGoogle}`);
 
