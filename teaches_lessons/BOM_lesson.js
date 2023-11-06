55e17d034f54e05448b796cfbb694a1ebaef9186
Casper <clydenberg@gmail.com>
2023-11-06 12:11:44 -0600

    defused the bomb

---
+++ teaches_lessons/BOM_lesson.js
@@ -0,0 +1,30 @@
+
+/*IIFE or also known as MAIN Method*/
+/*This automatically invokes functions within it*/
+(() => {
+
+    // setInterval() & clearInterval() methods
+    // let countdown = 10;
+    // console.log(countdown);
+    // const countdownTimer = setInterval(()=>{
+    //     countdown--;
+    //     console.log(countdown);
+    //     if(countdown === 0){
+    //         clearInterval(countdownTimer);
+    //         console.log("done!");
+    //     }
+    // }, 1000);
+
+    // setTimeout() Method
+    // const adPopup = setTimeout(()=>{
+    //     console.log(`PIKABOO!`);
+    // }, 5000);
+
+    /*Location Object*/
+    // console.log(window.location);
+    // window.location = "https://www.google.com";
+
+
+
+
+})();
