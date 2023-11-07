04dfa2107d9551c6c5b1f76019aeeef74d2fc0d6
Casper <clydenberg@gmail.com>
2023-11-07 09:04:33 -0600

    making sure my works pushed

---
+++ teaches_lessons/DOM_lesson.js
@@ -0,0 +1,11 @@
+(()=>{
+
+    /*we are saving a node to a variable so we can apply methods to it*/
+    const pageHeader = document.querySelector(`header.container h1`);
+
+    /*example of node method being applied to a saved node*/
+    pageHeader.style.color = `red`;
+
+
+
+})();
